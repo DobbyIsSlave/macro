88b3ba51dba7aca79af770b24e95308f6b0b3536
Eric <eric@orfeosound.com>
2020-04-17 17:00:27 +0900

    define 추가

--- macro/macro/model/Constants.cs
+++ macro/macro/model/Constants.cs
@@ -8,9 +8,11 @@ namespace macro
 {
     public static class Constants
     {
+        public const string mainFilePath = "C:\\macro\\macro\\macro\\ahk\\Maple.ahk";
+
+        public const string mapleFilePath = "C:\\macro\\macro\\macro\\ahk\\Maple.ahk";
         public const string doingFilePath = "C:\\macro\\macro\\macro\\ahk\\Doing.ahk";
         public const string blasterFilePath = "C:\\macro\\macro\\macro\\ahk\\Blaster.ahk";
-        public const string mapleFilePath = "C:\\macro\\macro\\macro\\ahk\\Maple.ahk";
         public const string classDDFilePath = "C:\\macro\\macro\\macro\\ahk\\Class_DD.ahk";
     }
 }
